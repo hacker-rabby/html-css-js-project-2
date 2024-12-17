da5228520723ca0600b4dd9e1be0d0e45bef1d24
hacker-rabby <mdfazlerabby868@gmail.com>
2024-12-17 21:49:00 +0000

    fixed partially

---
+++ Checking/index.js
@@ -0,0 +1,17 @@
+
+
+// const myElement = document.querySelector("#para");
+//     if(myElement){
+//         myElement.innerHTML="para is changed";
+//     }
+
+
+const paraElement=document.querySelector("#para");
+if(paraElement)
+{
+    paraElement.innerHTML="Paragraph is changed";
+    console.log(paraElement);
+    
+}
+const fullName="Robert"
+console.log(fullName)
