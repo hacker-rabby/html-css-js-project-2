0ca70efe5e456d22e08df06c016458d07b1ebc70
hacker-rabby <mdfazlerabby868@gmail.com>
2024-12-29 16:44:33 +0000

    added random emojies

---
+++ 21.Random Emojies/app.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("#emoji")
+
+const emojis=[
+    "🤣",
+    "😊",
+    "😂",
+    "❤️",
+    "😍",
+    "😒",
+    "👌",
+    "😘",
+    "💕",
+    "😁",
+    "👍",
+    "🙌",
+    "😉",
+    "😊",
+    "😋",
+    "😎",
+    "😙",
+    "🤩",
+    "🤗",
+    "🤐",
+    "🥱",
+    "😴",
+]
+
+btn.addEventListener('mouseover',()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+})
